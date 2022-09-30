058fb7f7a65261558b88ed6ba5f1c77cf587f8db
KNobles <nobles-k@hotmail.com>
2022-09-30 15:22:03 +0200

    MultiThreading Works... Somehow

--- Isidora_scraper.py
+++ Isidora_scraper.py
@@ -10,11 +10,9 @@ import re
 from requests import Session
 from bs4 import BeautifulSoup
 
-options = webdriver.FirefoxOptions()
 # options.headless = True
 
-driver = webdriver.Firefox(options=options)
-driver.headless = True
+
 # driver.implicitly_wait(10)
 root_url = "https://www.immoweb.be/en"
 search_apartment_url = root_url + "/search/apartment/for-sale" 
@@ -23,38 +21,41 @@ search_house_url = root_url + "/search/house/for-sale"
 
 
 
-def get_max_pages():
-# # Filter the empty items from the list and then change the list as a "set" so the duplicate are automatically removed
-    page_list = [elem.text for elem in driver.find_elements(By.XPATH, "//*[contains(text(), 'Page')]")]
-# # Get all elements of the page that has the word "Page" and put it in a list
-    page_list = set((list(filter(None, page_list))))
-    page_regex = re.compile("Page+\s")
-    max = 0
-    for string in page_list:
-        string = page_regex.sub('', string)
-        if max < int(string):
-            max = int(string)
-    return max
+# def get_max_pages():
+# # # Filter the empty items from the list and then change the list as a "set" so the duplicate are automatically removed
+#     page_list = [elem.text for elem in driver.find_elements(By.XPATH, "//*[contains(text(), 'Page')]")]
+# # # Get all elements of the page that has the word "Page" and put it in a list
+#     page_list = set((list(filter(None, page_list))))
+#     page_regex = re.compile("Page+\s")
+#     max = 0
+#     for string in page_list:
+#         string = page_regex.sub('', string)
+#         if max < int(string):
+#             max = int(string)
+#     return max
 
 
 def search_property_urls(i):
-    start_time = perf_counter() 
+    start_time = perf_counter()
 
+    options = webdriver.FirefoxOptions()
+    driver = webdriver.Firefox(options=options)
+    driver.headless = True
+    
     print(f"PAGE N°{i}")
-    search_url = search_apartment_url
-    driver.get(search_url + f"?page={i}")
+    driver.get(search_apartment_url + f"?page={i}")
     elements = driver.find_elements(By.XPATH, '//h2[@class="card__title card--result__title"]')
-
     items = []
     for item in elements:
+        # print(item.find_element(By.CLASS_NAME, "card__title-link").get_attribute("href"))
         items.append(item.find_element(By.CLASS_NAME, "card__title-link").get_attribute("href"))
     print(f"\nTime spent inside the loop: {perf_counter() - start_time} seconds.")
-    return items
+    driver.close()
 
 with get_context("fork").Pool() as pool:
     gen = tuple(pool.map(search_property_urls, range(1,16)))
 
-#driver.close()
+print(gen)
 
 #0.8927087783813477 seconds
 # search_apartments(search_apartment_url, driver)
