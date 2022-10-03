799b2277c91acc32aa9d9ea042846faa0ba6a26c
KNobles <nobles-k-@hotmail.com>
2022-10-03 15:45:48 +0200

    Added few getters in class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.url_scraper import Scraper
 from utils.collect_property_data import Property
+import pandas
 
 print("========================================================")
 print("\tWELCOME TO THE COLLECTING DATA CHALLENGE")
@@ -15,8 +16,12 @@ print("========================================================\n")
     # else:
         # print("Answer needs to be [Y] or [n]\n")
 
-prop = Property("https://www.immoweb.be/en/classified/duplex/for-sale/gent/9000/10143801?searchId=63399cae2b8c2")
-print(vars(prop))
-# print(prop.property_locality)
+# prop = Property("https://www.immoweb.be/en/classified/duplex/for-sale/gent/9000/10143801?searchId=63399cae2b8c2")
 # print(prop.property_type)
 # print(prop.property_sub_type)
+# print(prop.property_building_state)
+
+prop = Property("https://www.immoweb.be/en/classified/new-real-estate-project-apartments/for-sale/chaumont-gistoux/1325/10143472?searchId=63399cae2b8c2")
+print(prop.property_sub_type)
+# print(prop.property_locality)
+# print(prop.property_type)
--- utils/collect_property_data.py
+++ utils/collect_property_data.py
@@ -1,13 +1,14 @@
+from collections import defaultdict
 from selenium import webdriver
 
 class Property:
-
     def __init__(self, property_url) -> None:
         options = webdriver.FirefoxOptions()
         options.headless = True 
-        driver = webdriver.Firefox(options=options)
-        driver.get(property_url)
-        property_data = driver.execute_script("return window.classified")
+        self.driver = webdriver.Firefox(options=options)
+        self.driver.get(property_url)
+        property_data = self.property_data()
+
         self.property_type = property_data["property"]["type"]
         self.property_sub_type = property_data["property"]["subtype"]
         self.property_locality = property_data["property"]["location"]["locality"]
@@ -15,12 +16,40 @@ class Property:
         self.property_price = property_data["price"]["mainValue"]
         self.property_room_count = property_data["property"]["bedroomCount"]
         self.property_surface = property_data["property"]["netHabitableSurface"]
-        self.property_is_kitchen_fully_equipped = 1 if property_data["property"]["kitchen"]["type"] == "HYPER_EQUIPPED" else 0
+        self.property_is_kitchen_fully_equipped = self.property_is_kitchen_fully_equipped()
         self.property_is_furnished = 1 if property_data["transaction"]["sale"]["isFurnished"] == True else 0
         self.porperty_has_open_fire = 1 if property_data["property"]["fireplaceExists"] == True else 0
-        self.property_has_terrace = property_data["property"]["hasTerrace"]
-        self.property_has_garden = property_data["property"]["hasGarden"]
+        self.property_has_terrace = 1 if property_data["property"]["hasTerrace"] == True else 0
+        self.property_has_garden = 1 if property_data["property"]["hasGarden"] == True else 0
         # MISSING SURFACE OF THE LAND AND THE SURFACE OF THE PLOT AREA, WILL DO IT
-        self.property_facade_count = property_data["property"]["building"]["facadeCount"]
-        self.property_has_swimming_pool = property_data["property"]["hasSwimmingPool"]
-        self.property_building_state = property_data["property"]["building"]["condition"]
+        self.property_facade_count = self.property_data()
+        self.property_has_swimming_pool = 1 if property_data["property"]["hasSwimmingPool"] == True else 0
+        # self.property_building_state = property_data["property"]["building"]["condition"]
+    
+    def some_test(self, data):
+        try:
+            newdata = data
+        except TypeError:
+            newdata = None
+        return newdata
+
+
+    def property_facade_count(self):
+        try:
+            facade_count = self.property_data()["property"]["building"]["facadeCount"]
+        except TypeError:
+            return None
+
+    def property_data(self):
+        return self.driver.execute_script("return window.classified")
+
+    def property_is_kitchen_fully_equipped(self):
+        try:
+            has_kitchen = self.property_data()["property"]["kitchen"]["type"]
+            if has_kitchen == "HYPER_EQUIPPED":
+                return 1
+            else:
+                return 0
+        except TypeError:
+            return None
+
