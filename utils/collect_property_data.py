723bdf3d53c754289f1ae4e7eec98dfc70c11dc6
KNobles <nobles-k@hotmail.com>
2022-10-06 14:17:33 +0200

    Class is scraping well

--- utils/collect_property_data.py
+++ utils/collect_property_data.py
@@ -9,6 +9,9 @@ class Property:
         self.property_data = json_obj
         self.id = self.id()
         self.locality = self.locality()
+        self.postal_code = self.postal_code()
+        self.province = self.province()
+        self.region = self.region()
         self.type = self.type()
         self.sub_type = self.sub_type()
         self.sale_type = self.sale_type()
@@ -16,97 +19,168 @@ class Property:
         self.bedroom_count = self.bedroom_count()
         self.surface = self.surface()
         self.is_kitchen_fully_equipped = self.is_kitchen_fully_equipped()
-        self.is_furnished = 1 if self.property_data["transaction"]["sale"]["isFurnished"] == True else 0
-        self.has_open_fire = 1 if self.property_data["property"]["fireplaceExists"] == True else 0
-        self.has_terrace = 1 if self.property_data["property"]["hasTerrace"] == True else 0
-        self.has_garden = 1 if self.property_data["property"]["hasGarden"] == True else 0
-        self.terrace_surface = self.property_data["property"]["terraceSurface"] if self.property_data["property"]["terraceSurface"] == True else "None"
-        self.garden_surface = self.property_data["property"]["gardenSurface"] if self.property_data["property"]["gardenSurface"] == True else "None"
+        self.kitchen_type = self.kitchen_type()
+        self.is_furnished = self.is_furnished()
+        self.has_open_fire = self.has_open_fire()
+        self.has_terrace = self.has_terrace()
+        self.has_garden = self.has_garden()
+        self.terrace_surface = self.terrace_surface()
+        self.garden_surface = self.garden_surface()
         self.land_surface = self.land_surface()
         self.facade_count = self.facade_count()
-        self.has_swimming_pool = 1 if self.property_data["property"]["hasSwimmingPool"] == True else 0
+        self.has_swimming_pool = self.has_swimming_pool()
         self.building_state = self.building_state()
-
-    # def property_data(self, property_url):
-    #     with Session() as sess:
-    #         req = sess.get(property_url, timeout=3)
-    #         soup = BeautifulSoup(req.content, "html.parser")
-    #         script = soup.find('script',attrs={"type" :"text/javascript"})
-    #     return json.loads(script.contents[0][33:-10])
     
     def id(self):
         property_id = self.property_data["id"]
         return property_id
 
+    def garden_surface(self)-> int:
+        garden_surface = self.property_data["property"]["gardenSurface"]
+        if garden_surface is None:
+            return -1
+        return garden_surface
+
+    def terrace_surface(self)-> int:
+        terrace_surface = self.property_data["property"]["terraceSurface"]
+        if terrace_surface is None:
+            return -1
+        return terrace_surface
+
+    def has_terrace(self)-> int:
+        has_terrace = self.property_data["property"]["hasTerrace"]
+        val = self.convert_boolean(has_terrace)
+        return val
+
+    def has_open_fire(self)-> int:
+        has_open_fire = self.property_data["property"]["fireplaceExists"]
+        val = self.convert_boolean(has_open_fire)
+        return val
+
+    def is_furnished(self)-> int:
+        is_furnished = self.property_data["transaction"]["sale"]["isFurnished"]
+        val = self.convert_boolean(is_furnished)
+        return val
+
+    def has_garden(self)-> int:
+        has_garden = self.property_data["property"]["hasGarden"]
+        val = self.convert_boolean(has_garden)
+        return val
+
+    def is_kitchen_fully_equipped(self)-> int:
+        try:
+            is_kitchen_fully_equipped = self.property_data["property"]["kitchen"]["type"]
+            if is_kitchen_fully_equipped is None:
+                is_kitchen_fully_equipped = -1
+            elif is_kitchen_fully_equipped == "HYPER_EQUIPPED" or is_kitchen_fully_equipped == "USA_HYPER_EQUIPPED":
+                is_kitchen_fully_equipped = 1
+        except TypeError:
+            is_kitchen_fully_equipped = -1
+        return is_kitchen_fully_equipped
+
+    def has_swimming_pool(self)-> int:
+        has_swimming_pool = self.property_data["property"]["hasSwimmingPool"]
+        val = self.convert_boolean(has_swimming_pool)
+        return val
+
     def surface(self):
         surface = self.property_data["property"]["netHabitableSurface"]
         if surface is None:
-            surface = "None"
+            surface = -1
         return surface
 
     def price(self):
         price = self.property_data["price"]["mainValue"]
         if price is None:
-            price = "None"
+            price = -1
         return price
 
     def sale_type(self):
         sale_type = self.property_data["price"]["type"]
         if sale_type is None:
-            sale_type = "None"
+            sale_type = "NO_INFO"
         return sale_type
     
     def sub_type(self):
         sub_type = self.property_data["property"]["subtype"]
         if sub_type is None:
-            sub_type = "None"
+            sub_type = "NO_INFO"
         return sub_type
 
     def type(self):
         type = self.property_data["property"]["type"]
         if type is None:
-            type = "None"
+            type = "NO_INFO"
         return type
 
     def locality(self):
         locality = self.property_data["property"]["location"]["locality"]
         if locality is None:
-            locality = "None"
+            locality = "NO_INFO"
         return locality
+    
+    def postal_code(self)-> int:
+        postal_code = self.property_data["property"]["location"]["postalCode"]
+        if postal_code is None:
+            return -1
+        return postal_code
 
-    def bedroom_count(self):
+    def province(self):
+        province = self.property_data["property"]["location"]["province"]
+        if province is None:
+            return "NO_INFO"
+        return province
+
+    def region(self):
+        region = self.property_data["property"]["location"]["region"]
+        if region is None:
+            region = "NO_INFO"
+        return region
+
+    def bedroom_count(self)-> int:
         bedroom_count = self.property_data["property"]["bedroomCount"]
         if bedroom_count is None:
-            bedroom_count = "None"
+            bedroom_count = -1
         return bedroom_count
 
-    def land_surface(self):
+    def land_surface(self)-> int:
         try:
             land_surface = self.property_data["property"]["land"]["surface"]
+            if land_surface is None:
+                land_surface = -1
         except TypeError:
-            land_surface = "None"
+            land_surface = -1
         return land_surface
 
     def building_state(self):
         try:
             building_state = self.property_data["property"]["building"]["condition"]
+            if building_state is None:
+                building_state = "NO_INFO"
         except TypeError:
-            building_state = "None"
+            building_state = "NO_INFO"
         return building_state
 
-    def facade_count(self):
+    def facade_count(self)-> int:
         try:
             facade_count = self.property_data["property"]["building"]["facadeCount"]
+            if facade_count is None:
+                facade_count = -1
         except TypeError:
-            facade_count = "None"
+            facade_count = -1
         return facade_count
 
-    def is_kitchen_fully_equipped(self):
+    def kitchen_type(self):
         try:
             has_kitchen = self.property_data["property"]["kitchen"]["type"]
-            if has_kitchen == "HYPER_EQUIPPED":
-                return 1
-            else:
-                return 0
+            if has_kitchen is None:
+                has_kitchen = "NO_INFO"
         except TypeError:
-            return "None"
+            has_kitchen = "NO_INFO"
+        return has_kitchen
+    
+    def convert_boolean(self, answer)-> int:
+        match answer:
+            case True: return 1
+            case False: return 0
+            case None: return -1
--- utils/data_dictionary_prints_csv.py
+++ utils/data_dictionary_prints_csv.py
@@ -11,12 +11,16 @@ class PropertyDataWriter:
     columns = [
         "id",
         "locality",
+        "postal_code",
+        "region",
+        "province",
         "type_of_property",
         "subtype_of_property",
         "type_of_sale",
         "price",
         "number_of_bedrooms",
         "surface",
+        "kitchen_type",
         "fully_equipped_kitchen",
         "furnished",
         "open_fire",
@@ -36,6 +40,7 @@ class PropertyDataWriter:
         properties_url = pd.read_csv(file_location, encoding="utf-8")
         sess = Session()
         print(len(properties_url))
+        print(type(properties_url))
         for property_url in properties_url.values:
             # print(property_url[0])
             try :
@@ -51,12 +56,16 @@ class PropertyDataWriter:
                 property = Property(json.loads(script.contents[0][33:-10]))
                 property_data_dictionary["id"].append(property.id)
                 property_data_dictionary["locality"].append(property.locality)
+                property_data_dictionary["postal_code"].append(property.postal_code)
+                property_data_dictionary["region"].append(property.region)
+                property_data_dictionary["province"].append(property.province)
                 property_data_dictionary["type_of_property"].append(property.type)
                 property_data_dictionary["subtype_of_property"].append(property.sub_type)
                 property_data_dictionary["type_of_sale"].append(property.sale_type)
                 property_data_dictionary["price"].append(property.price)
                 property_data_dictionary["number_of_bedrooms"].append(property.bedroom_count)
                 property_data_dictionary["surface"].append(property.surface)
+                property_data_dictionary["kitchen_type"].append(property.kitchen_type)
                 property_data_dictionary["fully_equipped_kitchen"].append(property.is_kitchen_fully_equipped)
                 property_data_dictionary["furnished"].append(property.is_furnished)
                 property_data_dictionary["open_fire"].append(property.has_open_fire)
@@ -71,5 +80,5 @@ class PropertyDataWriter:
         return property_data_dictionary
 
     def write_data_to_file():
-        data_file = pd.DataFrame(PropertyDataWriter.map_property_to_data_dictionary("realestate_urls.csv"),columns=PropertyDataWriter.columns)
-        data_file.to_csv("Property_structured_data.csv")
+        data_file = pd.DataFrame(PropertyDataWriter.map_property_to_data_dictionary("realestate_urls2.csv"),columns=PropertyDataWriter.columns)
+        data_file.to_csv("Property_structured_data2.csv")
